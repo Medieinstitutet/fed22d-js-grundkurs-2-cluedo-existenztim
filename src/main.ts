b2ea39ac6de118e356bbe05be4e890803d9d3fda
existenztim <tim920129@hotmail.com>
2022-12-05 14:01:56 +0100

    :tada: fixed some ts/eslint errors.

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,10 @@ import './style/style.scss';
 
 // All kod härifrån och ner är bara ett exempel för att komma igång
 
-// I denna utils-fil har vi lagrat funktioner som ofta används, t.ex. en "blanda array"-funktion
-import { shuffle } from './utils';
-
 // I denna fil har vi lagrat vår "data", i detta exempel en ofullständig kortlek
 import exampleCardDeck from './exampleArray';
+// I denna utils-fil har vi lagrat funktioner som ofta används, t.ex. en "blanda array"-funktion
+import shuffle from './utils';
 
 // Blanda kortleken
 const myShuffledCardDeck = shuffle(exampleCardDeck);
